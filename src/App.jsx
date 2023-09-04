55d2309dce0fa47d25c97acc59f08f8387a51038
Sudeep <sdreple@gmail.com>
2023-09-04 21:39:26 +0545

    Create(): created navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Bot from "./components/bot";
+import Navbar from "./components/navbar";
 
 
 
 function App() {
   return <div className="">
+    <Navbar />
     <Bot />
   </div>;
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="">
+                <div className="mx-7">
+                    <h4 className="text-4xl uppercase font-bold">
+                        Su<span className="hover:text-cyan-500 transition duration-150 ease-in hover:ease-in-out ">deep</span>
+                    </h4>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;

9dc80937170ef8e19c2f9c00e06c5cc8efca97f5
Sudeep <sdreple@gmail.com>
2023-09-11 20:52:28 +0545

    Refactor(): updated code for responsiveness

--- src/components/bot.jsx
+++ src/components/bot.jsx
@@ -7,7 +7,7 @@ const Bot = () => {
             <ion-icon name="logo-instagram"></ion-icon>
             <ion-icon name="body-outline"></ion-icon>
             </div>
-            <div className="fixed flex items-center justify-center rounded-full bottom-5 sm:right-8 right-4 z-[999] text-white cursor-pointer text-4xl bg-[brown] hover:bg-cyan-500 w-16 h-16 animate-bounce">
+            <div className="fixed flex items-center justify-center rounded-full bottom-5 sm:right-8 right-4 z-[999] text-white cursor-pointer sm:text-4xl text-3xl bg-[brown] hover:bg-cyan-500 sm:w-16 w-14 sm:h-16 h-14 animate-bounce">
                 <ion-icon name="logo-octocat"></ion-icon>
             </div>
         </div>

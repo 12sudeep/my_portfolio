984eea11ce944a7a4348cf8ab04ba8853db42e67
Sudeep <sdreple@gmail.com>
2023-09-16 14:20:37 +0545

    Added(): responsiveness added

--- src/components/layouts/title.jsx
+++ src/components/layouts/title.jsx
@@ -1,12 +1,14 @@
 const Title = ({ title, desc }) => {
     return (
         <div className="flex flex-col gap-4 mb-14">
-            <h3 className="text-sm uppercase text-white tracking-wide">
+            <h3 className="text-3xl md:text-4xl font-semibold uppercase text-white hover:text-cyan-400 tracking-wide">
                 {title}
             </h3>
-            <h1>
+            <h1 className="text-xl md:text-2xl text-gray-400 my-3 capitalize">
                 {desc}
             </h1>
         </div>
     )
 }
+
+export default Title;

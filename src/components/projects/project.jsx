fa59f2a9532a7647592a107ff064beca7b6be6de
Sudeep <sdreple@gmail.com>
2023-09-20 23:40:54 +0545

    Added(): project.jsx's complete code added with responsiveness

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import Title from '../layouts/title'
-import ProjectCard from './projectCard'
-import img1 from "..//../assets/hospital-management-system-fb.png"
-import img2 from "..//../assets/uni.jpg"
-import img3 from "..//../assets/face_attendence.jpg"
-import img4 from "..//../assets/ds.webp"
+import React from 'react';
+import Title from '../layouts/title';
+import ProjectCard from './projectCard';
+import img1 from '..//../assets/hospital.webp';
+import img2 from '..//../assets/uni.jpeg';
+import img3 from '..//../assets/attend.jpeg';
+import img4 from '..//../assets/data.jpeg';
 
 const Project = () => {
     return (
-        <div className='container mx-auto min-h-screen flex lg:flex-col md:pt-2 pt-20 flex-col sm:px-28 px-8 items-center text-center'>
-            <div><Title title="Projects" desc="some projects done by me" />
-            </div>
-            <div className='grid lg:grid-cols-2 grid-cols gap-20 items-center'>
-                <ProjectCard src={img1} />
-                <ProjectCard src={img2} />
-                <ProjectCard src={img3} />
-                <ProjectCard src={img4} />
-
+        <div className='container mx-auto min-h-screen flex flex-col text-center justify-center pt-20 px-8 py-20'>
+            <Title title='Projects' desc='some projects done by me' />
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-16 px-2 sm:px-10'>
+                <ProjectCard src={img1} name='Hospital management system' />
+                <ProjectCard src={img2} name='University Ranking system' />
+                <ProjectCard src={img3} name='Face attendance project' />
+                <ProjectCard src={img4} name='Data science website' />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Project
+export default Project;

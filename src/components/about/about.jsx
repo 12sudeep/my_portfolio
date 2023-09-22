7595322779d549d6edb602481bca49651c126841
Sudeep <sdreple@gmail.com>
2023-09-22 22:29:52 +0545

    Refactor(): code refactored and responsiveness added

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,53 +1,48 @@
-import CV from "../../assets/SudeepFullel_cv.pdf"
-import Don from "../../assets/image_1.png"
+import CV from "../../assets/SudeepFullel_cv.pdf";
+import Don from "../../assets/image_1.png";
 import Title from "../layouts/title";
 
 const About = () => {
-
     const info = [
         { text: "Degree", count: "DataScience" },
         { text: "Completed Projects", count: "5+" },
         { text: "College", count: "BCU" },
-    ]
+    ];
 
-    return <section id="about" className="py-10 text-white">
-        <div className="text-center mt-8">
-            <Title title="About Me" desc="My intoduction" />
-            <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
-                <div>
-                    <div className="flex flex-col px-16 justify-center text-gray-300 my-4">
-                        <p className="sm:text-xl text-sm">
-                            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-                        </p>
-                        <div className="flex mt-10 justify-center gap-7">
-                            {
-                                info.map(col => (
+    return (
+        <section id="about" className="sm:py-20 py-10 text-white">
+            <div className="text-center mt-8">
+                <Title title="About Me" desc="My introduction" />
+                <div className="flex flex-col-reverse lg:flex-row items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+                    <div className="md:mt-0 mt-8">
+                        <div className="flex flex-col px-4 md:px-16 text-gray-300 sm:my-4 text-center md:text-left">
+                            <p className="sm:text-xl text-sm">
+                                It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
+                            </p>
+                            <div className="flex mt-10 md:justify-start justify-center gap-7">
+                                {info.map((col) => (
                                     <div key={col.text}>
                                         <h3 className="md:text-xl text-1xl font-semibold text-white">{col.count}</h3>
                                         <span className="">{col.text}</span>
                                     </div>
-
-                                ))
-                            }
+                                ))}
+                            </div>
+                            <br />
+                            <br />
+                            <a href={CV} download>
+                                <button className="btn-primary hover:scale-95 sm:mt-5">Download CV</button>
+                            </a>
                         </div>
-                        <br />
-                        <br />
-                        <a href={CV} download>
-                            <button className="btn-primary mt-5">Download CV</button>
-                        </a>
+                    </div>
+                    <div className="flex-1 flex justify-center lg:pb-32 pb-0 justify-items-center">
+                        <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm">
+                            <img src={Don} alt="don" className="w-full" />
+                        </div>
                     </div>
                 </div>
-                <div className="flex-1 flex justify-center md:pb-20">
-                    <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm">
-                        <img src={Don} alt="don" className="w-full" />
-                    </div>
-                </div>
-
             </div>
-
-        </div>
-
-    </section>
+        </section>
+    );
 };
 
 export default About;

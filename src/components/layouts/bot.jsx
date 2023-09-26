ba2e91992ccfd0c8d443f8e5bfa020dd1086a3c3
Sudeep <sdreple@gmail.com>
2023-09-26 08:21:04 +0545

    Refactor(): linked bot with blog section

--- src/components/layouts/bot.jsx
+++ src/components/layouts/bot.jsx
@@ -1,4 +1,43 @@
+import React, { useState } from 'react';
+import Popup from '../blog/popup';
+import blog1 from "../../assets/data.jpeg"
+import blog2 from "../../assets/attend.jpeg"
+
+
+
+
+
 const Bot = () => {
+    const [isPopupVisible, setPopupVisible] = useState(false);
+    const [selectedBlog, setSelectedBlog] = useState(null);
+
+    // Sample blog data
+    const blogs = [
+        {
+            name: 'Blog 1',
+            image: {blog1},
+            url: '#',
+        },
+        {
+            name: 'Blog 2',
+            image: {blog2},
+            url: '#',
+        },
+    ];
+
+    const openPopup = () => {
+        setSelectedBlog(null);
+        setPopupVisible(true);
+    };
+
+    const closePopup = () => {
+        setPopupVisible(false);
+    };
+
+    const handleGoToClick = (url) => {
+        window.location.href = url;
+    };
+
     return (
         <div className="container mx-auto">
             <div className="fixed flex flex-col gap-6 items-center justify-left bottom-[48px] sm:left-8 left-4 z-[999] text-white sm:text-2xl text-xl ">
@@ -7,9 +46,15 @@ const Bot = () => {
                 <div className="cursor-pointer transition duration-300 ease-in-out hover:scale-125"><ion-icon name="logo-instagram"></ion-icon></div>
                 <ion-icon name="body-outline"></ion-icon>
             </div>
-            <div className="fixed flex items-center justify-center rounded-full bottom-5 sm:right-8 right-4 z-[999] text-white cursor-pointer sm:text-4xl text-3xl bg-[brown] hover:bg-cyan-500 sm:w-16 w-14 sm:h-16 h-14 animate-bounce">
+            <button
+                className="fixed flex items-center justify-center rounded-full bottom-5 sm:right-8 right-4 z-[999] text-white cursor-pointer sm:text-4xl text-3xl bg-[brown] hover:bg-cyan-500 sm:w-16 w-14 sm:h-16 h-14 animate-bounce"
+                onClick={openPopup}
+            >
                 <ion-icon name="logo-octocat"></ion-icon>
-            </div>
+            </button>
+            {isPopupVisible && (
+                <Popup onClose={closePopup} blogs={blogs} onGoToClick={handleGoToClick} />
+            )}
         </div>
     );
 };

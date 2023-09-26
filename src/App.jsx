bbf794ee45a6448bd74181cdac5ff5a709059177
Sudeep <sdreple@gmail.com>
2023-09-26 09:00:44 +0545

    Refactor(): changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import About from "./components/about/about";
-import Bot from "./components/home/bot";
+import Bot from "./components/layouts/bot";
 import Hero from "./components/home/hero";
 import Navbar from "./components/navbar/navbar";
 import Resume from "./components/resume/resume";
+import Contact from "./components/contact/contact";
+import Footer from "./components/footer/footer";
+import Project from "./components/projects/project";
 
 
 
 function App() {
-  return <div className="container mx-auto">
+
+
+
+
+  return <div className="container mx-auto w-full">
     <Router>
       <Navbar />
       <Bot />
       <Hero />
       <About />
+      <Project />
       <Resume />
+      <Contact />
+      <Footer />
     </Router>
   </div>;
 }
